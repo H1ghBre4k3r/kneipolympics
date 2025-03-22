9b79a8b22e89fc208337b691100da2f51ad37c96
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2025-03-22 17:00:34 +0100

    feat: display user details in AdminRoute component
    Fetch and display user details in the AdminRoute component. 
    Utilize the useAdmin hook to retrieve user data and render 
    a list of users with their names and preferences, enhancing 
    the admin interface for better user management.

--- frontend/src/routes/admin.tsx
+++ frontend/src/routes/admin.tsx
@@ -1,5 +1,23 @@
-export function AdminRoute() {
-  // const _admin = useAdmin();
+import { useAdmin } from "../hooks/useAdmin";
 
-  return <h3>Admin</h3>;
+export function AdminRoute() {
+  const { users } = useAdmin();
+
+  return (
+    <>
+      <h3>Admin</h3>
+      <ul>
+        {users.map((user) => {
+          const { prefs, name } = user;
+          const { firstName, lastName, phone } = prefs;
+
+          return (
+            <li>
+              {name}, {firstName}, {lastName}, {phone}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
