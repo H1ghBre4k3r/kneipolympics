0beaa824d448e38c6ca1d064f29f9dec56f07501
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2025-03-22 17:10:14 +0100

    feat: add TL;DR section to RegisteredIndexRoute
    Add a TL;DR section to the RegisteredIndexRoute component to 
    provide users with quick information about an upcoming event. 
    Style the section in the CSS to improve layout and spacing.

--- frontend/src/routes/registered.index.tsx
+++ frontend/src/routes/registered.index.tsx
@@ -7,6 +7,21 @@ export function RegisteredIndexRoute() {
   return (
     <section>
       <h3>Hello {name}!</h3>
+      <section id="tldr">
+        <div>At some point you'll be able to find more information here.</div>
+
+        <h4>TL;DR:</h4>
+        <div>
+          <b>Date:</b> 26. April
+        </div>
+        <div>
+          <b>Where:</b> Kiel
+        </div>
+        <div>
+          <b>What:</b> Kneipolympics
+        </div>
+      </section>
+
       <Link to="/logout">Logout</Link>
     </section>
   );
