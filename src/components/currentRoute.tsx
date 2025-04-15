e280a6f6eef2fa8989b41982d081700ea71ea54a
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2025-04-15 15:21:24 +0200

    feat: display a different component if a user has a route

---
+++ src/components/currentRoute.tsx
@@ -0,0 +1,7 @@
+export function CurrentRoute() {
+  return (
+    <section id="current-route">
+      <h4>FOO</h4>
+    </section>
+  );
+}
--- src/routes/current.tsx
+++ src/routes/current.tsx
@@ -1,13 +1,21 @@
+import { CurrentRoute } from "../components/currentRoute";
+import { useAuth } from "../hooks/useAuth";
 import { useLabels } from "../hooks/useLabels";
 
 export function CurrentGameRoute() {
   const l = useLabels();
+  const { user } = useAuth();
+  const { route } = user?.prefs ?? {};
   return (
     <section>
       <h3>{l("currentGame")}</h3>
-      <section id="tldr">
-        <p>{l("furtherInformation")}</p>
-      </section>
+      {!route || route === "---" ? (
+        <section id="tldr">
+          <p>{l("furtherInformation")}</p>
+        </section>
+      ) : (
+        <CurrentRoute />
+      )}
     </section>
   );
 }
