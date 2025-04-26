9b0ed9bf6afaf27b895f984b3a0e1227df273f2b
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2025-04-26 15:40:02 +0200

    fix: remove condoms

--- src/routes/registered.index.tsx
+++ src/routes/registered.index.tsx
@@ -3,7 +3,6 @@ import { useAuth } from "../hooks/useAuth";
 import { useAdmin } from "../hooks/useAdmin";
 import { HendriksMegaButton } from "../components/hendriksMegaButton";
 import { Scoreboard } from "../components/scoreboard";
-import { LogoLarge } from "../components/logoLarge";
 
 export function RegisteredIndexRoute() {
   const { isAdmin } = useAdmin();
@@ -11,7 +10,6 @@ export function RegisteredIndexRoute() {
   const { name } = auth.user ?? {};
   return (
     <section>
-      <LogoLarge />
       <h3>Hello {name}!</h3>
 
       {isAdmin && <HendriksMegaButton />}
